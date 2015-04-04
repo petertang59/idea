6885f143be6d742c63429990f7334f85c69b479b
petertang59 <petertang59@gmail.com>
2015-04-04 15:33:07 -0700

    asd

--- serene-anchorage-8233/js/controller2.js
+++ serene-anchorage-8233/js/controller2.js
@@ -1,25 +1,26 @@
-var idea_KEY = 'Ideas';
+// var idea_KEY = 'Ideas';
 
-// IdeaApp.Ideas = [
-  // {'title':'This is a pretty cool idea!', 'detail':'Extremely awesome!', 'image':'img/typography.png'},
-  // {'title':'This is another really cool Idea!', 'detail':'But it involves beer','image':'img/webdesign.png'}
-// ];
+// IdeaApp.Ideas = (localStorage.getItem('Ideas')!==null) ? JSON.parse($scope.saved) :[
+IdeaApp.Ideas = [ 
+  {'title':'This is a pretty cool idea!', 'detail':'Extremely awesome!', 'image':'img/typography.png'},
+  {'title':'This is another really cool Idea!', 'detail':'But it involves beer','image':'img/webdesign.png'}
+];
 
-function loadidea() {
-  var idea = localStorage.getItem(idea_KEY);
-    try {
-      idea = JSON.parse(idea);
-      console.log(Object.prototype.toString.call(idea));
-      if (Object.prototype.toString.call(idea) !== '[object Array]') {
-        idea = [];
-      }
-    } catch (e) {
-      idea = [];
-    }
-    return idea;
-}
+// function loadidea() {
+//   var idea = localStorage.getItem(idea_KEY);
+//     try {
+//       idea = JSON.parse(idea);
+//       console.log(Object.prototype.toString.call(idea));
+//       if (Object.prototype.toString.call(idea) !== '[object Array]') {
+//         idea = [];
+//       }
+//     } catch (e) {
+//       idea = [];
+//     }
+//     return idea;
+// }
 
-// function pushPost(idea) {
+// function pushIdea(idea) {
 //   var idea = loadidea();
 //   idea.push(idea);
 //   // localStorage.setItem(idea_KEY, JSON.stringify(idea));
@@ -30,27 +31,28 @@ function loadidea() {
 //   localStorage.setItem(idea_KEY, '[]');
 // }
 
-IdeaApp.controller('IdeaController',['$scope',function($scope){
- $scope.Ideas = IdeaApp.Ideas;
+IdeaApp.controller('IdeaController',[
+'$scope',function($scope){
  $scope.saved = localStorage.getItem('Ideas');
- $scope.Ideas = [{'title':'This is a pretty cool idea!', 'detail':'Extremely awesome!', 'image':'img/typography.png'},
-  {'title':'This is another really cool Idea!', 'detail':'But it involves beer','image':'img/webdesign.png'} ];
-
+$scope.Ideas = (localStorage.getItem('Ideas')!==null) ? JSON.parse($scope.saved) : IdeaApp.Ideas;
+ // $scope.Ideas = [
+ // {'title':'This is a pretty cool idea!', 'detail':'Extremely awesome!', 'image':'img/typography.png'},
+ //  {'title':'This is another really cool Idea!', 'detail':'But it involves beer','image':'img/webdesign.png'} ];
+	console.log($scope.Ideas);
   // $scope.Ideas = (localStorage.getItem('Ideas')!==null) ? JSON.parse($scope.saved) : [{'title':'This is a pretty cool idea!', 'detail':'Extremely awesome!', 'image':'img/typography.png'},
   // {'title':'This is another really cool Idea!', 'detail':'But it involves beer','image':'img/webdesign.png'} ];
-  localStorage.setItem('todos', JSON.stringify($scope.todos));
+  localStorage.setItem('Ideas', JSON.stringify($scope.Ideas));
 
 
   $scope.addIdea = function (){
    $scope.Ideas.push({'title':$scope.newIdea,'detail':$scope.newDetail, 'image':$scope.newImage, toggle3: false})
+   // IdeaApp.push({'title':$scope.newIdea,'detail':$scope.newDetail, 'image':$scope.newImage, toggle3: false});
    $scope.newIdea = ""
    $scope.newDetail = ""
    $scope.newImage = ""
    // IdeaApp.Ideas = $scope.Ideas;
    localStorage.setItem('Ideas', JSON.stringify($scope.Ideas));
     console.log($scope.Ideas);
-
-    // IdeaApp.push({'title':$scope.newIdea,'detail':$scope.newDetail});
   }  
 
   $scope.clearCompleted = function (){
@@ -62,7 +64,18 @@ IdeaApp.controller('IdeaController',['$scope',function($scope){
   $scope.delete = function  (index) {
    $scope.Ideas.splice(index, 1);
    console.log($scope.Ideas);
+   localStorage.setItem('Ideas', JSON.stringify($scope.Ideas));
   };
+
+  $scope.archive = function() {
+		var oldIdeas = $scope.Ideas;
+		$scope.Ideas = [];
+		angular.forEach(oldIdeas, function(idea){
+			if (!idea.done)
+				$scope.Ideas.push(idea);
+		});
+		localStorage.setItem('Ideas', JSON.stringify($scope.Ideas));
+	};
    
   }]);
 
