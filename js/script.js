83901bb1a761a649be2ab0047f026cec2d7ca45f
petertang59 <petertang59@gmail.com>
2015-04-05 00:36:09 -0700

    asdf

--- js/script.js
+++ js/script.js
@@ -1,9 +1,41 @@
-var myElement = document.getElementById('myElement');
-var hammer = new Hammer.Manager(myElement);
-var swipe = new Hammer.Swipe();
+// var myElement = document.getElementById('myElement');
+// var hammer = new Hammer.Manager(myElement);
+// var swipe = new Hammer.Swipe();
 
-hammer.add(swipe);
+// hammer.add(swipe);
 
-hammer.on('swipeleft', function (){
-	console.log('Hello');
+// hammer.on('swipeleft', function (){
+// 	console.log('Hello');
+// });
+
+var container = document.querySelector('.container');
+var msnry = new Masonry( container, {
+  // options
+  columnWidth: 50%,
+  itemSelector: '.pin-box'
 });
+
+// $(document).ready(function(){
+
+//   // hide #back-top first
+//   $("#back-top").hide();
+  
+//   // fade in #back-top
+//   $(function () {
+//     $(window).scroll(function () {
+//       if ($(this).scrollTop() > 100) {
+//         $('#back-top').fadeIn();
+//       } else {
+//         $('#back-top').fadeOut();
+//       }
+//     });
+
+//     // scroll body to 0px on click
+//     $('#back-top a').click(function () {
+//       $('body,html').animate({
+//         scrollTop: 0
+//       }, 700);
+//       return false;
+//     });
+//   });
+// });
--- views/pinterest.php
+++ views/pinterest.php
@@ -1,6 +1,14 @@
 <link rel="stylesheet" type="text/css" href="inspiration.css"/>
-
-<div class="container" ng-class="{'container-wide':toggle.state2}">
+<a id="top"></a>
+<div class="js-masonry container" ng-class="{'container-wide':toggle.state2}"
+  data-masonry-options='{ 'columnWidth': 40%, 'itemSelector': '.pin-box' }'>
+		    <div id="back-top">
+		        <a>
+		          <div id="top-arrow1"></div>
+		          <div id="top-arrow2"></div>
+		        <div id="back-top-bg"></div>
+		        </a>
+      		</div>
     <div class="button" ng-click="toggle.state2 = !toggle.state2">Change View</div>
 <ul ng-repeat="element in pinterestFeed">
   <li class="pin-box">
@@ -11,3 +19,31 @@
     <button ng-click="storeImage(element)">Save</button></li>
 </ul>
 </div>
+
+<script>
+$(document).ready(function(){
+
+  // hide #back-top first
+  $("#back-top").hide();
+  
+  // fade in #back-top
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('#back-top').fadeIn();
+      } else {
+        $('#back-top').fadeOut();
+      }
+    });
+
+    // scroll body to 0px on click
+    $('#back-top').click(function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 700);
+      return false;
+    });
+  });
+});
+
+</script>
